6a50ca2ad3bda2fb8c853b6c6c7e59c817963b1c
turbolybo <andreas_lybo@hotmail.com>
2017-03-30 12:06:08 +0200

    Start web

---
+++ header.php
@@ -0,0 +1,12 @@
+<header>
+   <div class="navigation"></div>   
+<nav id="nav">
+   <div id="mobile-menu">≡</div>
+   <div class="logo"></div>
+      <li>HJEM</li>
+      <li>FJERDINGEN</li>
+      <li>VULKAN</li>
+      <li>BRENNERIVEIEN</li>
+   </ul>
+</nav>
+</header>
---
+++ index.php
@@ -0,0 +1,9 @@
+<!doctype HTML>
+<head>
+   <link rel="stylesheet" type="text/css" href="css/body.css">
+   <link rel="stylesheet" type="text/css" href="css/header.css">
+   <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+</head>
+<body>
+   <?php include_once 'header.php'; ?>
+</body>
