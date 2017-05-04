927e853a8780dd1c04231f192fd0cd28163377be
Sondre Røisehagen <sondre.roisehagen@gmail.com>
2017-05-04 09:37:14 +0200

    localhost
    oppdatert så localhost fungerer hos meg

--- php/local-query.php
+++ php/local-query.php
@@ -4,15 +4,23 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 
 require './vendor/autoload.php';
 
+$config = include("config.php");
+
+if($config['debug']==true){
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+}
+
 $capsule = new Capsule();
 
 $capsule->addConnection([
-    'driver' => 'mysql',
-    'host' => 'localhost',
-    'port' => 3306,
-    'database' => 'eksamen',
-    'username' => 'root',
-    'password' => '',
+    'driver' => $config["driver"],
+    'host' => $config["host"],
+    'port' => $config["port"],
+    'database' => $config["database"],
+    'username' => $config["username"],
+    'password' => $config["password"],
 
 ]);
 $capsule->bootEloquent();
