dc569430b1df987665dec43d2fb80f7a5f27313d
Sondre Røisehagen <sondre.roisehagen@gmail.com>
2017-05-18 12:37:36 +0200

    startet på log inn i footer

--- php/footer.php
+++ php/footer.php
@@ -26,5 +26,45 @@
             <a href="./skole.php?skoleId=1"><li>Vulkan</li></a>
         </ul>
    </div>
-   <div id="footer-bottom">Copyright © 2017 - Eksamensgruppe 22. Alle rettigheter forbeholdes. <a href="admin.php">Admin</a></div>
+
+    <div id="footer-bottom">Copyright © 2017 - Eksamensgruppe 22. Alle rettigheter forbeholdes.
+        <button1 onclick="document.getElementById('id01').style.display='block'" style="width:auto;"><a>Logg inn Admin</a></button1></div>
 </footer>
+
+<div id="id01" class="modal">
+
+    <form class="modal-content animate" action="admin.php">
+        <div class="imgcontainer">
+            <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
+            <img src="http://ambisjoner.no/wp-content/uploads/2015/01/Westerdals-logo1-1024x696.jpg" alt="Avatar" class="avatar">
+        </div>
+
+        <div class="container">
+            <label><b>Brukernavn</b></label>
+            <input type="text" placeholder="Skriv inn brukernavn" name="uname" required>
+
+            <label><b>Passord</b></label>
+            <input type="password" placeholder="Skriv inn passord" name="psw" required>
+
+            <button type="submit">Login</button>
+            <input type="checkbox" checked="checked"> Husk meg
+        </div>
+
+        <div class="container" style="background-color:#f1f1f1">
+            <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Avbryt</button>
+            <span class="psw">Glemt <a href="#">passord?</a></span>
+        </div>
+    </form>
+</div>
+
+<script>
+    // Get the modal
+    var modal = document.getElementById('id01');
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+</script>
