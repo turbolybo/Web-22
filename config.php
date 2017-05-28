5fab790be0546444061f3c9faf52f1fac83d6318
turbolybo <andreas_lybo@hotmail.com>
2017-05-28 16:04:10 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+    return array(
+        'driver' => 'mysql',
+        'host' => 'localhost',
+        'port' => 3306,
+        'database' => 'eksamen',
+        'username' => 'root',
+        'password' => '',
+        'debug' => true,
+    )
+?>
